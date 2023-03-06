f43549f813fe90b38728894bd30f9f8e3d172287
Your Name <jlmeeks587@gmail.com>
2023-03-06 12:30:03 -0500

    Card changes to thank you card once submitted

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const card = document.querySelector('.card');
+const thankYouCard = document.querySelector('.thank-you-card');
 const selectionCircle = document.querySelector('.selection-circle');
+const submitButton = document.querySelector('button');
 
-selectionCircle.addEventListener('onmouseover', ((id) => {
-  console.log(selectionCircle.id);
-}))
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  card.style.display = 'none';
+  thankYouCard.style.display = 'flex';
+})
