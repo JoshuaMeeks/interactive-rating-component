7e217694470674222a0d40965a2a277719154280
Your Name <jlmeeks587@gmail.com>
2023-03-06 16:00:48 -0500

    Added pointer cursors to selection circles and button

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const card = document.querySelector('.card');
 const thankYouCard = document.querySelector('.thank-you-card');
-const selectionCircle = document.querySelector('.selection-circle');
+const selectionContainer = document.querySelector('.selection-container');
+const selectionCircles = document.querySelectorAll('.selection-circle');
 const submitButton = document.querySelector('button');
+
+console.log(selectionCircles)
+
+selectionCircles.forEach((circle) => {
+  circle.addEventListener('mouseover', () => {
+    circle.style.backgroundColor = 'hsl(217, 12%, 63%)'
+  })
+})
 
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
+  setTimeout(() => {
+  card.style.display = 'flex';
+  thankYouCard.style.display = 'none';
+  }, 3000)
   card.style.display = 'none';
   thankYouCard.style.display = 'flex';
 })
